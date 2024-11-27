102de3b6fbda701b558846ff95c204497ed78ff9
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2024-11-27 20:43:01 +0100

    using new neoforge template

--- src/main/java/BetterPipes/BlockPipe.java
+++ src/main/java/BetterPipes/BlockPipe.java
@@ -45,7 +45,7 @@ public class BlockPipe extends Block implements EntityBlock {
     @Override
     protected List<ItemStack> getDrops(BlockState state, LootParams.Builder params) {
         List<ItemStack> drops = new ArrayList<>();
-        drops.add(new ItemStack(PIPE.get(),1));
+        drops.add(new ItemStack(this,1));
         return drops;
     }
     @Override
